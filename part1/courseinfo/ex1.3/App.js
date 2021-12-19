20f83deadf355b473e5b4a7732bb2e557c9edc9e
Macwhel <60788935+Macwhel@users.noreply.github.com>
2021-12-18 23:10:05 -0800

    Create App.js

---
+++ part1/courseinfo/ex1.3/App.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <div>
+      <p>{props.course}</p>
+    </div>
+  )
+}
+
+
+const Part = (props) => {
+  return (
+    <div>
+      {props.part} {props.exercise}
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part = {props.part1.name} exercise = {props.part1.exercises} />
+      <Part part = {props.part2.name} exercise = {props.part2.exercises} />
+      <Part part = {props.part3.name} exercise = {props.part3.exercises} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content part1 = {part1} part2 = {part2} part3 = {part3}/>
+      <Total exercises1 = {part1.exercises} exercises2 = {part2.exercises} exercises3 = {part3.exercises}/>
+    </div>
+  )
+}
+
+export default App;
