602e4e221ba9b024e2b770d213d6400549b43b05
Macwhel <60788935+Macwhel@users.noreply.github.com>
2022-01-02 19:47:34 -0800

    Create App.js

---
+++ part2/ex2.10/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+import Filter from './components/Filter'
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const Person = {
+      name : newName,
+      number: newNumber
+    }
+
+    const alreadyAdded = persons.some((p) => JSON.stringify(p.name) === JSON.stringify(Person.name))
+
+    if (alreadyAdded){
+      window.alert(`${newName} is already added to phonebook`)
+    }
+    else {
+      setPersons(persons.concat(Person))
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const personsToShow = (filter === '') ? persons : persons.filter(p => 
+    p.name.toUpperCase().includes(filter.toUpperCase()) === true
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filterChange={handleFilterChange} />
+      <h2>Add a new</h2>
+      <PersonForm addPerson = {addPerson} name = {handleNameChange} number = {handleNumberChange} />
+      <h2>Numbers</h2>
+      <Persons personsToShow = {personsToShow} />
+    </div>
+  )
+}
+
+export default App
