7abe94702c16a84f73281496c91ae575d1f52f80
Macwhel <60788935+Macwhel@users.noreply.github.com>
2022-01-11 15:04:09 -0800

    Create Filter.js

---
+++ part2/ex2.15/components/Filter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Filter = (props) => {
+
+    return (
+        <form>
+            <div>
+                filter shown with <input onChange = {props.filterChange} />
+            </div>
+        </form>
+    )
+}
+
+export default Filter
